e37c7003fbea5e48490bed506b9380870b373543
sonalgoel1789 <68201093+sonalgoel1789@users.noreply.github.com>
2020-07-13 13:55:11 +0530

    Added Test Case 2
    test_case2 calculates second test scenario

--- Unit_Test.py
+++ Unit_Test.py
@@ -13,7 +13,18 @@ class PromoTest(unittest.TestCase):
         bought_ob = Bought(['A', 'B', 'C', 'D'], [5, 5, 1, 2])  # {item:no. of items}
         final_amt = bought_ob.apply_promo(promos_ob, sku_ob)
 
-        self.assertEqual(final_amt, 400)
+        self.assertEqual(final_amt, 395)
+    def test_case2(self):
+        sku_ob = SKU(['A', 'B', 'C', 'D'], [50, 30, 20, 15])
+        sku = sku_ob.create_mp()
+
+        promos_ob = Promos([['A'], ['B'], ['C', 'D']], [[3], [2], [1, 1]], [[130], [45], [30]])
+        promos = promos_ob.consolidate  # [items] [no. of items] [promo_cost]
+
+        bought_ob = Bought(['A', 'B', 'C', 'D'], [1, 1, 1, 0])  # {item:no. of items}
+        final_amt = bought_ob.apply_promo(promos_ob, sku_ob)
+
+        self.assertEqual(final_amt, 100)
 
 
 if __name__ == '__main__':
