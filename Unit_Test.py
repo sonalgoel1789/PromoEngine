8efa890ee480e8a0514568c5c6ce133157e0737e
sonalgoel1789 <68201093+sonalgoel1789@users.noreply.github.com>
2020-07-13 14:03:15 +0530

    Added Test case 3
    Added a test scenario where no items are bought

--- Unit_Test.py
+++ Unit_Test.py
@@ -25,6 +25,17 @@ class PromoTest(unittest.TestCase):
         final_amt = bought_ob.apply_promo(promos_ob, sku_ob)
 
         self.assertEqual(final_amt, 100)
+    def test_case3(self):
+        sku_ob = SKU(['A', 'B', 'C', 'D'], [50, 30, 20, 15])
+        sku = sku_ob.create_mp()
+
+        promos_ob = Promos([['A'], ['B'], ['C', 'D']], [[3], [2], [1, 1]], [[130], [45], [30]])
+        promos = promos_ob.consolidate  # [items] [no. of items] [promo_cost]
+
+        bought_ob = Bought(['A', 'B', 'C', 'D'], [0, 0, 0, 0])  # {item:no. of items}
+        final_amt = bought_ob.apply_promo(promos_ob, sku_ob)
+
+        self.assertEqual(final_amt, 0)
 
 
 if __name__ == '__main__':
